92dcf86ef4e6dcb4d6cc01cee35093de2dcf9898
Raúl Muñoz <raul@mothership.com>
2023-02-10 18:46:49 +0100

    added ios project to make development easier

---
+++ ios/RCTMGL-v10/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rutappa
+//
+//  Created by admin on 21/10/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>{
+
+}
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ ios/RCTMGL-v10/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Rutappa
+//
+//  Created by admin on 21/10/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
